ca0c222418bf46c26fb1b9040b7d1da6fbb459f5
ariannacostanzo <costanzo.arianna@outlook.it>
2024-03-19 16:21:54 +0100

    creating crud rooutes

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,18 @@ Route::get('/', GuestHomeController::class)->name('guest.home');
 
 Route::get('/admin', AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
 
+//rotte per projects
 Route::get('/admin/projects', [ProjectController::class, 'index'])->name('admin.projects.index');
+Route::get('/admin/projects/create', [ProjectController::class, 'create'])->name('admin.projects.create');
+Route::get('/admin/projects/{project}', [ProjectController::class, 'show'])->name('admin.projects.show');
+Route::post('/admin/projects', [ProjectController::class, 'store'])->name('admin.projects.store');
+Route::get('/admin/projects/{project}/edit', [ProjectController::class, 'edit'])->name('admin.projects.edit');
+Route::put('/admin/projects/{project}', [ProjectController::class, 'update'])->name('admin.projects.update');
+Route::delete('/admin/projects/{project}', [ProjectController::class, 'destroy'])->name('admin.projects.destroy');
 
 
 
+//rotte auth
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -32,4 +40,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
