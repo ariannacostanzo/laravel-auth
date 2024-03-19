bcbe051a2c0ea38dbd317dabeefee3cc2c91aec7
ariannacostanzo <costanzo.arianna@outlook.it>
2024-03-19 16:34:45 +0100

    creating table columns

--- database/migrations/2024_03_19_145540_create_projects_table.php
+++ database/migrations/2024_03_19_145540_create_projects_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+
+            //creo le colonne base per un progetto, dunque titolo, descrizione, slug e immagine
+            $table->string('title', 50)->unique();
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->string('image')->nullable();
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
