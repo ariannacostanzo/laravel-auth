316c94ac0162217aca25a188b6d7cc92fee264aa
ariannacostanzo <costanzo.arianna@outlook.it>
2024-03-20 19:08:27 +0100

    creting slug in form

--- resources/js/form/image_preview.js
+++ resources/js/form/image_preview.js
@@ -10,3 +10,14 @@ inputImage.addEventListener('input', () => {
     previewContainer.src = inputImage.value ? inputImage.value : emptyPlaceholder
         
 })
+
+
+//slug 
+const slugContainer = document.getElementById("slug");
+const titleContainer = document.getElementById("title");
+
+
+slugContainer.value = titleContainer.value.trim().split(' ').join('-');
+titleContainer.addEventListener('blur', () => {
+    slugContainer.value = titleContainer.value.trim().split(" ").join("-");
+})
