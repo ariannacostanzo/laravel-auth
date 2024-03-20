f1582a558809db2a1c7086e17c3fc035d0e0c679
ariannacostanzo <costanzo.arianna@outlook.it>
2024-03-20 16:55:03 +0100

    created delete modal

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -32,28 +32,32 @@
     </thead>
     <tbody>
         @forelse ($projects as $project)
-        {{-- modal --}}
-            <div class="modal fade" id="modal{{$project->id}}" tabindex="-1" aria-hidden="true">
+            {{-- modal --}}
+            <div class="modal fade" id="modal{{ $project->id }}" tabindex="-1" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="modal{{$project->id}}Label">Sei sicuro di voler eliminare questo
-                                progetto?</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                aria-label="Close"></button>
+                            <h1 class="modal-title fs-5" id="modal{{ $project->id }}Label">
+                              Sei sicuro di voler eliminare questo progetto?
+                              </h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
                             {{ $project->title }}
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                            {{-- todo elimina --}}
-                            <button type="button" class="btn btn-primary">Conferma</button>
+                            <form action="{{ route('admin.projects.destroy', $project->id) }}", method="POST"
+                                class="delete-form">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-primary">Conferma</button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-          {{-- modal --}}
+            {{-- modal --}}
             <tr>
                 <th scope="row">{{ $project->id }}</th>
                 <td>{{ $project->title }}</td>
@@ -65,17 +69,10 @@
                                 class="fa-regular fa-eye"></i></a>
                         <a href="{{ route('admin.projects.edit', $project->id) }}" class="btn btn-warning"><i
                                 class="fa-solid fa-pen"></i></a>
-                        {{-- <form action="{{route('admin.projects.destroy', $project->id)}}", method="POST" class="delete-form">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger"><i class="fa-regular fa-trash-can"></i></button>
-                      </form> --}}
-                        {{-- todo  --}}
                         <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                            data-bs-target="#modal{{$project->id}}">
+                            data-bs-target="#modal{{ $project->id }}">
                             <i class="fa-regular fa-trash-can"></i></button>
                         </button>
-                        {{-- todo  --}}
                     </div>
                 </td>
             </tr>
